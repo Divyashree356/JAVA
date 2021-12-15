2121d6c6d3cac1101acbdf36bfa76f33f71a9aef
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2021-12-15 22:47:25 +0530

    Create The State Of Wakanda - 1.java

---
+++ The State Of Wakanda - 1.java
@@ -0,0 +1,60 @@
+//Sample Input
+3x4
+11
+12
+13
+14
+21
+22
+23
+24
+31
+32
+33
+34
+
+//Sample Output
+11
+21
+31
+32
+22
+12
+13
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int[][] arr = new int[n][m];
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++) {
+        arr[i][j] = scn.nextInt();
+      }
+    }
+    for (int i = 0; i < m; i++)
+    {
+      if (i % 2 == 0)
+      {
+        for (int j = 0; j < n; j++)
+        {
+          System.out.println(arr[j][i]);
+        }
+      }
+      else {
+        for (int j = n - 1; j >= 0; j--)
+        {
+          System.out.println(arr[j][i]);
+        }
+      }
+    }
+  }
+
+}
