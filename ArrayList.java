145d8971a0ca52637149cbea025ba7be8f0ff8ef
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-01-17 01:48:46 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,43 @@
+//Sample Input
+4
+3 12 13 15
+  
+//Sample Output
+[12, 15]
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static boolean isPrime(int val)
+    {   
+        for(int i=2;i*i<=val;i++)
+        {
+            if(val%i==0){
+            return false;}
+        }
+        return true;
+    }
+	public static void solution(ArrayList<Integer> al){
+		for(int i=al.size()-1;i>=0;i--)
+		{
+		    int val=al.get(i);
+		    if(isPrime(val))
+		    {
+		        al.remove(i);
+		    }
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+
+}
