f978ea43d3defb8d7420b4110298dd41e79d6d4d
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-01-22 22:07:55 +0530

    Create Power Linear.java

---
+++ Power Linear.java
@@ -0,0 +1,27 @@
+Sample Input
+2
+5
+Sample Output
+32
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        int ans=power(x ,n);
+        System.out.println(ans);
+    }
+
+    public static int power(int x, int n){
+        if(n==0)
+        return 1;
+        
+        return x*power(x,n-1);
+    }
+
+}
