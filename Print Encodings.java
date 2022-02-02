8e49032105e821eb1aa0845435b98cb58158e4fe
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-02-02 19:09:35 +0530

    Create Print Encodings.java

---
+++ Print Encodings.java
@@ -0,0 +1,47 @@
+Sample Input
+655196
+
+Sample Output
+feeaif
+feesf
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        String s= sc.next();
+        printEncodings(s,"");
+        
+    }
+
+    public static void printEncodings(String str, String ans) 
+    {
+        if(str.length()==0)
+        {
+            System.out.println(ans);
+            return;
+        }
+        if(str.charAt(0)=='0')
+         return;
+          
+        char ch1= str.charAt(0);
+        int v1 = ch1-'0';
+        
+        printEncodings(str.substring(1), ans+ (char)(v1+'a'-1));
+        
+        if(str.length()>1)
+        {
+            char ch2= str.charAt(1);
+            int v2= ch2-'0';
+            
+            int num= v1*10+v2;
+            if(num <=26)
+             printEncodings(str.substring(2), ans+(char)(num+'a'-1));
+        }
+    }
+
+}
