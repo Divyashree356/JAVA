0560bc801ef945e89f4f28d786138cb47306d177
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-03-06 17:28:36 +0530

    Create Highest Frequency Character.java

---
+++ Highest Frequency Character.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.next();
+        HashMap<Character,Integer> hm= new HashMap<>();
+        for(int i=0;i<str.length();i++)
+        {
+            char ch =str.charAt(i);
+            // if(hm.containsKey(str.charAt(i))==false)
+            // {
+            //     hm.put(str.charAt(i) , 1);
+            // }
+            // else
+            //     hm.put(str.charAt(i),hm.get(str.charAt(i))+1);
+            hm.put(ch , hm.getOrDefault(ch , 0)+1);
+        }
+
+
+        char max=str.charAt(0);
+        for(char key : hm.keySet())
+        {
+            if(hm.get(key) >hm.get(max))
+              max=key;
+        }
+        System.out.println(max);
+
+    }
+
+}
