8b28be336a852c6bad5823f9fbcae23a44991720
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-06-07 20:19:02 +0530

    Create Minimum Wire Required To Connect All Pcs.java

---
+++ Minimum Wire Required To Connect All Pcs.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   static class pp{
+      int src;
+      int parent;
+      int wt;
+      pp(){}
+      pp(int src , int parent , int wt)
+      {
+         this.src=src;
+         this.parent=parent;
+         this.wt=wt;
+      }
+   }
+
+   public static void primAlgo( ArrayList<Edge>[] graph)
+   {
+      PriorityQueue<pp> pq= new PriorityQueue<>((a,b)->{
+         return a.wt-b.wt;
+      });
+
+      int n=graph.length;
+      boolean[] vis= new boolean[n];
+
+      pq.add(new pp(0,-1,0));
+
+
+      while(pq.size()!=0)
+      {
+         pp rem= pq.remove();
+
+         if(rem.parent!=-1 && !vis[rem.src])
+         {
+            System.out.println("["+ rem.src+"-"+rem.parent+"@"+rem.wt+"]");
+         }
+         vis[rem.src]=true;
+       
+
+         for(Edge e: graph[rem.src])
+         {
+            int nbr= e.nbr;
+            
+            if(!vis[nbr])
+             pq.add(new pp(nbr , rem.src, e.wt));
+         }
+         
+      
+      
+      }
+      
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+    primAlgo(graph);
+  
+   }
+
+}
